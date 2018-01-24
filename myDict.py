5a3999933d4ba3d3f55c73f8f2a74a8d3f3138ba
lijiemac <lijie@boco.com.cn>
2018-01-24 10:49:41 +0800

    5

--- myDict.py
+++ myDict.py
@@ -9,18 +9,23 @@ __author__ = 'Jacklee'
 #import logging
 
 class Dict(dict):
-	def __init__(self, **dw):
-		super().__init__(**dw)
+    '''
+        命名字典类型
+        可以使用字典
+    '''
+    def __init__(self, **dw):
+        dict.__init__(**dw)
 
-	def __getattr__(self, attr):
-		try:
-			return self[attr]
-		except KeyError:
-			raise AttributeError('Dict对象没有属性: %s' % attr)
+    def __getattr__(self, attr):
+        try:
+            return self[attr]
+        except KeyError:
+            raise AttributeError('Dict对象没有属性: %s' % attr)
 
-	def __setattr__(self, attr, value):
-		self[attr] = value
+    def __setattr__(self, attr, value):
+        self[attr] = value
 
-d = Dict(a = 1, b = 2)
-print(d['a'])
-print(d.a)
+
+MYDICT = Dict(a=1, b=2)
+print(MYDICT['a'])
+print(MYDICT.a)
